183c4831e2bcc2d70f8990c918f9971063e69dcb
tlsrs@iscte-iul.pt <tlsrs@iscte-iul.pt@758c9aaa-d4b3-4ce3-ba81-68bafac83749>
2013-09-17 16:30:16 +0000

    fixed launcher

--- WebLauncher/src/web/Launcher.java
+++ WebLauncher/src/web/Launcher.java
@@ -1,66 +1,32 @@
 package web;
 
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 
 public class Launcher {
-
-
-	private String attribute;
-	private String className;
-
-	public Launcher(String attribute, String className) {
-		this.attribute = attribute;
-		this.className = className;
-	}
-
 
 	private void init() {
 
 		try {
 //			URL url = new URL("http://localhost:8080/WebRunner.jar");
 //			URL url = new URL("http://10.40.50.96:8080/WebRunner.jar");
-			URL url = new URL("http://evolve.dcti.iscte.pt/WebRunner.jar");
+			URL url = new URL("http://evolve.dcti.iscte.pt:8080/worker.jar");
 			URL[] urls = new URL[]{url};
 			System.out.println("waiting for classes...");
 			ClassLoader loader = new URLClassLoader(urls);
-
-			Class<?> cls = loader.loadClass(className);
+			Class<?> cls = loader.loadClass("screensaver.MainScreensaver");
 			System.out.println("got classes... ");
 			WebRunner runner = (WebRunner)(cls.newInstance());
-			runner.setAttribute(attribute);
 			runner.init();
 
-
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
 	}
 
 
 	public static void main(String[] args) {
-		if (args.length >= 2) {
-			String attribute= args[0];
-			String className = args[1];
-			for(String s :args){
-				System.out.println(s);
-			}
-			new Launcher(attribute, className).init();
-		} else {
-			System.out.println("Error: wrong number of arguments... use java web.Launcher attributes classname");
-		}
+		new Launcher().init();
 	}
 
 
